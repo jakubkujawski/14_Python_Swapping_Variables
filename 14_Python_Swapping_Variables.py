a606dee684a9c2fa0fe8aec442008164d117de43
Jakub Kujawski <jaahcob@gmail.com>
2024-10-08 23:05:00 +0100

    Add files via upload

---
+++ 14_Python_Swapping_Variables.py
@@ -0,0 +1,19 @@
+Python 3.12.5 (v3.12.5:ff3bc82f7c9, Aug  7 2024, 05:32:06) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # SWAP VARIABLES
+>>> 
+>>> a = 5
+>>> b = 6
+>>> 
+>>> print(a)
+5
+>>> print(b)
+6
+>>> 
+>>> a,b = b,a
+>>> 
+>>> print(a)
+6
+>>> 
+>>> print(b)
+5
